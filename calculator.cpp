ad283b998fd854cbb112d9b5a86aa441c79d4ab1
Adolfo Carrillo <gustavoadolfocarrillocamacho@gmail.com>
2024-06-19 16:15:36 -0600

    Clicking number buttons on display test, ok

--- calculator.cpp
+++ calculator.cpp
@@ -1,14 +1,72 @@
 #include "calculator.h"
 #include "ui_calculator.h"
 
-Calculator::Calculator(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::Calculator)
+// Holds current value of calculation
+double calcVal = 0.0;
+
+// Will define if this was the last math button clicked
+bool divTrigger = false;
+bool multTrigger = false;
+bool addTrigger = false;
+bool subTrigger = false;
+
+Calculator::Calculator(QWidget *parent):
+    QMainWindow(parent),
+    ui(new Ui::Calculator)
 {
     ui->setupUi(this);
+
+    // Put 0.0 in Display
+    ui->Display->setText(QString::number(calcVal));
+
+    // Will hold references to all the number buttons
+    QPushButton *numButtons[10];
+
+    // Cycle through locating the buttons
+    for(int i = 0; i < 10; ++i){
+        QString butName = "Button_" + QString::number(i);
+
+        // Get the buttons by name and add to array
+        numButtons[i] = Calculator::findChild<QPushButton *>(butName);
+
+        // When the button is released call num_pressed()
+        connect(numButtons[i], SIGNAL(released()), this,
+                SLOT(NumPressed()));
+    }
+
 }
 
 Calculator::~Calculator()
 {
     delete ui;
+}
+
+void Calculator::NumPressed(){
+
+    // Sender returns a pointer to the button pressed
+    QPushButton *button = (QPushButton *)sender();
+
+    // Get number on button
+    QString butVal = button->text();
+
+    // Get the value in the display
+    QString displayVal = ui->Display->text();
+
+    if((displayVal.toDouble() == 0) || (displayVal.toDouble() == 0.0)){
+
+        // calcVal = butVal.toDouble();
+        ui->Display->setText(butVal);
+
+    } else {
+        // Put the new number to the right of whats there
+        QString newVal = displayVal + butVal;
+
+        // Double version of number
+        double dblNewVal = newVal.toDouble();
+
+        // Set value in display and allow up to 16
+        // digits before using exponents
+        ui->Display->setText(QString::number(dblNewVal, 'g', 16));
+
+    }
 }
